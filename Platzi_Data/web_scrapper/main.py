ff2e68a8d5c0dc119cbadd7fa5df46e0ac7c9d79
esteban <esteban.londono.padilla@gmail.com>
2018-11-08 13:30:51 -0500

    pruebas

--- Platzi_Data/web_scrapper/main.py
+++ Platzi_Data/web_scrapper/main.py
@@ -8,12 +8,12 @@ logger = logging.getLogger(__name__)
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
-    logging.info('Beginning scraper for{}'.format(host))
+    logging.info('Beginning scraper for {}'.format(host))
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    news_site_choices= list(config()['new_sites'].keys())
+    news_site_choices= list(config()['news_sites'].keys())
     parser.add_argument('news_site',help='the New site that you want to scrape', type=str,choices=news_site_choices)
     args = parser.parse_args()
     _news_scraper(args.news_site)
